0902013606058c2d8fd141c00cc4d5420ae69787
lukaszjargielo <l.jargielo88@gmail.com>
2025-03-30 18:35:34 +0200

    Added dependencies for testing to POM.xml

---
+++ src/test/java/pl/horus/model/classes/FolderCabinetTest.java
@@ -0,0 +1,21 @@
+package pl.horus.model.classes;
+
+import org.junit.jupiter.api.Test;
+
+
+
+class FolderCabinetTest {
+
+    @Test
+    void findFolderByName() {
+
+    }
+
+    @Test
+    void findFoldersBySize() {
+    }
+
+    @Test
+    void count() {
+    }
+}
