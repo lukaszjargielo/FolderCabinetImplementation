1061a637343504ccbfd5be37dac81de10ee0b9ba
lukaszjargielo <l.jargielo88@gmail.com>
2025-03-30 17:01:19 +0200

    First simple implementation of Cabinet's interface methods

--- src/main/java/pl/horus/model/classe/FolderCabinet.java
+++ src/main/java/pl/horus/model/classe/FolderCabinet.java
@@ -4,7 +4,27 @@ import pl.horus.model.interfaces.Cabinet;
 import pl.horus.model.interfaces.Folder;
 
 import java.util.List;
+import java.util.Optional;
 
 public class FolderCabinet implements Cabinet {
     private List<Folder> folders;
+
+    @Override
+    public Optional<Folder> findFolderByName(String name) {
+        return folders.stream()
+                .filter(folder -> folder.getName().equalsIgnoreCase(name))
+                .findFirst();
+    }
+
+    @Override
+    public List<Folder> findFoldersBySize(String size) {
+        return folders.stream()
+                .filter(folder -> folder.getSize().equalsIgnoreCase(size))
+                .toList();
+    }
+
+    @Override
+    public int count() {
+        return folders.size();
+    }
 }
