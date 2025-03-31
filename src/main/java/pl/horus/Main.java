8d55907a52c88b3d20ceb952ad97567de3081f31
lukaszjargielo <l.jargielo88@gmail.com>
2025-03-31 18:39:41 +0200

    Final version of application

--- src/main/java/pl/horus/Main.java
+++ src/main/java/pl/horus/Main.java
@@ -4,6 +4,6 @@ package pl.horus;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Application works!");
+        System.out.println("Application works! It contains final version of code.");
     }
 }
